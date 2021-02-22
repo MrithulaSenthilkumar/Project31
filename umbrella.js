45ef955c2da0abb360626eda7c9111d0964c2083
MrithulaSenthilkumar <74912521+MrithulaSenthilkumar@users.noreply.github.com>
2021-02-22 10:51:17 -0500

    Add files via upload

---
+++ umbrella.js
@@ -0,0 +1,18 @@
+class Umbrella{
+     constructor(x,y){
+         var options={
+             isStatic:true,
+
+         }
+         this.body=Bodies.circle(x,y,50,options);
+         this.radius=50;
+         World.add(world,this.body);
+         this.image=loadImage("Walking Frame/walking_1.png");
+     }
+     display(){
+         var pos=this.body.position;
+         imageMode(CENTER);
+         image(this.image,pos.x,pos.y,300,300);
+
+     }
+}
